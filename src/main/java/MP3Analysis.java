4142d9e2ab46ad71d7e0f868e55e9d5d18019fda
mcholewa1218 <malgorzatacholewa1218@gmail.com>
2020-04-28 21:43:32 +0200

    saving to file added

--- src/main/java/MP3Analysis.java
+++ src/main/java/MP3Analysis.java
@@ -3,6 +3,7 @@ import com.mpatric.mp3agic.InvalidDataException;
 import com.mpatric.mp3agic.Mp3File;
 import com.mpatric.mp3agic.UnsupportedTagException;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.*;
 import java.util.ArrayList;
@@ -31,6 +32,15 @@ public class MP3Analysis {
             x.printStackTrace();
         }
 
+        try {
+            FileWriter fileWriter = new FileWriter(filePath + "\\description.txt");
+            for (Song song : listOfSongs){
+                fileWriter.write(song.toString() + "\n");
+            }
+            fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return listOfSongs;
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
     public static void main(String[] args) {
         MP3Analysis mp3Analysis = new MP3Analysis();
-        List<Song> listOfSongs = new ArrayList<>();
+        List<Song> listOfSongs;
         String filePath = "C:\\Users\\malgo\\Desktop\\mp3";
         listOfSongs = mp3Analysis.getListOfSongs(filePath);
         for (Song song : listOfSongs){
